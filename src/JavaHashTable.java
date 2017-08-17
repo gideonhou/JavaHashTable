8c467b2190ad687f825e3f5127fb8ef14206cbea
gideonhou <gideonhou@gmail.com>
2017-08-16 18:36:17 -0700

    Need to implement the methods that have enumerations

--- src/JavaHashTable.java
+++ src/JavaHashTable.java
@@ -5,8 +5,11 @@ import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.Vector;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * 
@@ -22,105 +25,164 @@ public class JavaHashTable<K, V> extends Dictionary<K, V> implements Map<K, V>, 
 	
 	private Set<K> keys; // set of keys. Use hashset
 	
-	private Collection<Map.Entry<Integer, LinkedList<V>>> buckets; // use ArrayList for this
-	/**
+	/*
+	 * buckets is a collection of entries in which the key is an Integer and the value is a linked list of entries.
+	 * The entries of the linked list are the actual keys and the value is the value.
+	 */
+	private Collection<Map.Entry<Integer, LinkedList<Map.Entry<K,V>>>> buckets; // use ArrayList for this
+	
+	/**	DONE
 	 * Constructs a new, empty hashtable with a default initial capacity (11) and load factor (0.75).
 	 */
 	public JavaHashTable() {
 		this.loadFactor = .75; // load factor of .75
 		this.keys = new HashSet<K>(11); // initializes a set with a capacity of 11
-		this.buckets = new ArrayList<Map.Entry<Integer, LinkedList<V>>>();
+		this.buckets = new ArrayList<Map.Entry<Integer, LinkedList<Map.Entry<K, V>>>>(11);
 	}
 	
-	/**
+	/**	DONE
 	 * Constructs a new, empty hashtable with the specified initial capacity and default load factor (0.75)
 	 * 
 	 * @param initialCapacity
 	 */
 	public JavaHashTable(int initialCapacity) {
-		
+		if(initialCapacity < 0) throw new IllegalArgumentException();
+		this.loadFactor = .75;
+		this.keys = new HashSet<K>(initialCapacity);
+		this.buckets = new ArrayList<Map.Entry<Integer, LinkedList<Map.Entry<K, V>>>>(initialCapacity);
 	}
 	
-	/**
+	/** DONE
 	 * Constructs a new, empty hashtable with the specified initial capacity and the specified load factor.
 	 * 
 	 * @param initialCapacity
 	 * @param loadFactor
 	 */
 	public JavaHashTable(int initialCapacity, float loadFactor) {
-		
+		if(initialCapacity < 0 || loadFactor < 0) throw new IllegalArgumentException();
+		this.loadFactor = loadFactor;
+		this.keys = new HashSet<K>(initialCapacity);
+		this.buckets = new ArrayList<Map.Entry<Integer, LinkedList<Map.Entry<K, V>>>>(initialCapacity);
 	}
 	
-	/**
+	/**	DONE
 	 * Constructs a new hashtable with the same mappings as the given Map.
 	 * 
 	 * @param t
 	 */
 	public JavaHashTable(Map<? extends K, ? extends V> t) {
-		
+		this.loadFactor = .75;
+		this.putAll(t);		
 	}
 	
-	/**
+	/**	DONE
 	 * Clears this hashtable so that it contains no keys.
 	 */
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		this.keys.clear();
+		this.buckets.clear();
 	}
 
-	/**
+	/** DONE
 	 * Tests if the specified object is a key in this hashtable.
+	 * 
+	 * O(number of keys in set)
 	 */
 	@Override
 	public boolean containsKey(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		if(arg0 == null) throw new NullPointerException();
+		return this.keys.contains(arg0);
 	}
 
-	/**
+	/** DONE
 	 * Returns true if this hashtable maps one or more keys to this value.
+	 * 
+	 * O(total number of linked list elements for every bucket)
 	 */
 	@Override
 	public boolean containsValue(Object arg0) {
-		// TODO Auto-generated method stub
+		if(arg0 == null) throw new NullPointerException();
+		for(Map.Entry<
+				Integer, LinkedList<
+				Map.Entry<K, V>>> entry : this.buckets
+				) { // entry is element of buckets(ArrayList)
+			LinkedList<Map.Entry<K, V>> ll = 
+					entry.getValue(); // ll is linked list
+
+			// I really want to use iterator here...
+			for(int i = 0; i < ll.size(); i++) { // iterate through linked list of entries. 
+				if (ll.get(i).getValue().equals(arg0)) return true; // return true if the entry's value matches
+			}
+		}
 		return false;
 	}
 
-	/**
+	/**DONE
 	 * Returns a Set view of the mappings contained in this map.
 	 */
 	@Override
 	public Set<java.util.Map.Entry<K, V>> entrySet() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<Map.Entry<K, V>> returnSet = new HashSet<Map.Entry<K, V>>();
+		
+		for(Map.Entry<
+				Integer, LinkedList<
+				Map.Entry<K, V>>> entry : this.buckets
+				) { // entry is element of buckets(ArrayList)
+			
+			returnSet.addAll((Collection<? extends Map.Entry<K, V>>) entry.getValue());	
+		}
+		return returnSet;
 	}
 
-	/**
+	/** DONE
 	 * Returns a Set view of the keys contained in this map.
 	 */
 	@Override
 	public Set<K> keySet() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.keys;
 	}
 
-	/**
+	/** DONE
 	 * Copies all of the mappings from the specified map to this hashtable.
+	 * 
+	 * O(number of elements in arg0)
+	 * 
+	 * we can cast because the elements of arg0 are subclasses of K and V
 	 */
 	@Override
 	public void putAll(Map<? extends K, ? extends V> arg0) {
-		// TODO Auto-generated method stub
-		
+		if(arg0 == null) throw new NullPointerException();
+		Set mappingsToPut = arg0.entrySet();
+		for(Map.Entry entry : arg0.entrySet()) {			
+			this.put( (K) entry.getKey(), (V) entry.getValue());
+		}
+		return;
 	}
 
-	/**
+	/** DONE
 	 * Returns a Collection view of the values contained in this map.
+	 * 
+	 * O(total number of entries in every bucket)
 	 */
 	@Override
 	public Collection<V> values() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<V> returnCollection = new ArrayList<V>();
+		
+		for(Map.Entry<
+				Integer, LinkedList<
+				Map.Entry<K, V>>> entry : this.buckets
+				) { // entry is element of buckets(ArrayList)
+			LinkedList<Map.Entry<K, V>> ll = 
+					entry.getValue(); // ll is linked list
+
+			// I really want to use iterator here...
+			for(int i = 0; i < ll.size(); i++) { // iterate through linked list of entries. 
+				returnCollection.add(ll.get(i).getValue());
+			}
+		}
+		return returnCollection;
 	}
 
 	/**
@@ -128,8 +190,9 @@ public class JavaHashTable<K, V> extends Dictionary<K, V> implements Map<K, V>, 
 	 */
 	@Override
 	public Enumeration<V> elements() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Enumeration<V> retEnum = new 
+		return retEnum;
 	}
 
 	/**
@@ -143,13 +206,13 @@ public class JavaHashTable<K, V> extends Dictionary<K, V> implements Map<K, V>, 
 		return null;
 	}
 
-	/**
+	/** DONE
 	 * Tests if this hashtable maps no keys to values.
 	 */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		if(this.keys.size() == 0) return true;
+		else return false;
 	}
 
 	/**
@@ -188,4 +251,23 @@ public class JavaHashTable<K, V> extends Dictionary<K, V> implements Map<K, V>, 
 		return 0;
 	}
 
+	private class JavaHashTableValueEnumeration implements Enumeration<V> {
+
+		protected JavaHashTableValueEnumeration() {
+			
+		}
+		
+		@Override
+		public boolean hasMoreElements() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public E nextElement() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
 }
